359e91cc2d184707ae210933e8f3f0ceae834af9
trajanovinicius <viniluk9@gmail.com>
2022-10-17 16:20:47 -0300

    creating relationship between manufacturer and product via sequelize

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 (async () => {
+
   const database = require('./db')
-  const Produto = require('./produto')
-  await database.sync();
+  const Produto = require('./models/produto')
+  const Fabricante = require('./models/fabricante')
+  await database.sync({force: true});
+
+  const novoFabricante = await Fabricante.create({
+    nome: 'Apple'
+  })
 
 //Agora posso usar o model Produto, para fazer alterações na tabela de produtos
 //sem precisar nada de sql.
 
-   const novoProduto = await Produto.create({
-    nome: 'Teclado USB',
-    preco: 250,
-    descricao: 'Teclado gamer'
+  const novoProduto = await Produto.create({
+    nome: 'MacBook',
+    preco: 10000,
+    descricao: 'notebook apple',
+    idFabricante: novoFabricante.id
    })
    console.log(novoProduto)
+
+
 //estou usando await porque operações no banco de dados são assíncronas.
 //e create é uma operação.
 
-const produto = await Produto.findByPk(4) // existem vários métodos para fazer operações no banco de dados
-console.log(produto)
+/*const produto = await Produto.findByPk(4) // existem vários métodos para fazer operações no banco de dados
+console.log(produto)*/
 
-await produto.destroy()
+//await Produto.destroy()
 
 })();
---
+++ models/fabricante.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const database = require('../db')
+
+
+const Fabricante = database.define('fabricante', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true, 
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome: {
+    type: Sequelize.STRING, 
+    allowNull: false,
+  },
+})
+
+module.exports = Fabricante
